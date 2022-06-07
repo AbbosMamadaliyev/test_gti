932428f98f9e8e31225cd1d0c3ac5fefe8e00872
HafizIbrohimXodjayev2002 <hafizibrohim2002@gmail.com>
2022-06-07 10:38:08 +0500

    add test file

---
+++ lib/test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class GitTest extends StatefulWidget {
+  const GitTest({Key? key}) : super(key: key);
+
+  @override
+  State<GitTest> createState() => _GitTestState();
+}
+
+class _GitTestState extends State<GitTest> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
